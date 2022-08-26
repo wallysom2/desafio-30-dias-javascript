d47b8b042eef0a957d840d8d0279447bf21624b4
Wallyson <wallysom2@gmail.com>
2022-08-26 15:19:45 -0300

    disafio 1

---
+++ 1-desafio.js
@@ -0,0 +1,15 @@
+
+const numbers = [4,6,2] //4
+
+function media (numbers) {
+
+    let soma = 0
+
+    for (let i = 0; i <= numbers.lenght; i++) {
+        soma = soma + numbers [i]
+        
+    }
+ 
+}
+
+media (numbers);
